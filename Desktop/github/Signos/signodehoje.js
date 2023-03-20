0cc8691def8e97acb67aafdc11f525ffaddd0b54
Isabellyconfessor <isabellysales4536@gmail.com>
2023-03-20 17:24:06 -0300

    final-commit

--- Desktop/github/Signos/signodehoje.js
+++ Desktop/github/Signos/signodehoje.js
@@ -29,8 +29,8 @@ const retorna_signo = (signos, data) => {
 
     for ( const signo of signos ){
 
-        let data_inicio_signo = new Date(ano + "-" + signo["DataInicio"] + "00:00:00");
-        let data_fim_signo = new Date(ano + "-" + signo["DataFim"] + "23:59:59");
+        let data_inicio_signo = new Date(ano + "-" + signo["DataInicio"] + " 00:00:00");
+        let data_fim_signo = new Date(ano + "-" + signo["DataFim"] + " 23:59:59");
 
         let tipo_comparacao = signo["DataInicio"] == "12-22" ? "or" : "and";
 
